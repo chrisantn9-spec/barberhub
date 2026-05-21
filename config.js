bdcda224c4ea56e7d9e1d228bac5b9bfd818ad8b
chrisantn9-spec <chris.antn9@gmail.com>
2026-05-20 21:52:10 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 // config.js
-// ⚠️ NUNCA subas claves secretas a GitHub
-// Usa variables de entorno en Vercel/Netlify
-
 const SUPABASE_URL = 'https://TU-PROYECTO.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_s2KUqB2eUZFtKFatxuQagQ_3HOQ24P0'; // ✅ Esta SÍ es pública
+const SUPABASE_ANON_KEY = 'sb_publishable_s2KUqB2eUZFtKFatxuQagQ_3HOQ24P0';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
