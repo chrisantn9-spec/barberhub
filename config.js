7a2aaf75b8f36fbd7cf69437a340cd1ea8f0c30e
chrisantn9-spec <chris.antn9@gmail.com>
2026-05-22 22:19:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const { createClient } = supabase;
 
 // 🔑 PEGA AQUÍ TU CLAVE PÚBLICA (anon) DE SUPABASE
-const SUPABASE_URL = 'https://qnlbaxxbsbwbgqucbywn.supabase.co';
+const SUPABASE_URL = 'https://'qnlbaxxbsbwbgqucbywn.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubGJheHhic2J3YmdxdWNieXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMzQzNjcsImV4cCI6MjA5NDcxMDM2N30.6n-0yf-9fuZKO8qJEM2dOmwBNxhijQtaBzVo2TNesjM'; 
 
 window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
